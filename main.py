91b0dc3f49604b8868bbd5514c67916ecd1d3fb4
Jonathon Quick <jonathon@spliff.li>
2023-02-06 16:44:17 +0000

    parses the date and time columns scraped from investing.com and combines them into a single datetime column in the df

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import datetime
+
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
@@ -8,8 +10,9 @@ options = Options()
 options.headless = False  # hide GUI
 options.add_argument("--window-size=1920,1080")  # set window size to native GUI size
 options.add_argument("start-maximized")  # ensure window is full-screen
+options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2}) # Load without images
 
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Chrome(options=options)
 
 
 def scrape(event_id: str):
@@ -21,12 +24,18 @@ def scrape(event_id: str):
     #   time.sleep(1)
     table = driver.find_element(By.ID, f"eventHistoryTable{event_id}")
 
-    df = pd.DataFrame(columns=['Release Date', 'Time', 'Prelim', 'Actual', 'Forecast', 'Previous'])
+    df = pd.DataFrame(columns=['Timestamp', 'Prelim', 'Actual', 'Forecast', 'Previous'])
     for row in table.find_elements(By.XPATH, ".//tbody/tr"):
         # print(row.find_element(By.XPATH, "./td[1]").text)
+        release_date = row.find_element(By.XPATH, "./td[1]").text
+        if ' (' in release_date:
+            release_date = release_date.split(" (")[0]
+
+        release_time = row.find_element(By.XPATH, "./td[2]").text
+        timestamp = datetime.datetime.strptime(f"{release_date} {release_time}".strip(), "%b %d, %Y %H:%M")
+
         df = df.append({
-            'Release Date': row.find_element(By.XPATH, "./td[1]").text,
-            'Time': row.find_element(By.XPATH, "./td[2]").text,
+            'Timestamp': timestamp,
             'Prelim': "Unknown",  # Add a check for this
             'Actual': row.find_element(By.XPATH, "./td[3]/span").text,
             'Forecast': row.find_element(By.XPATH, "./td[4]").text,
