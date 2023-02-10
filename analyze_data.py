e5b3497576510d7111b9bfa9fe765d66960f4a17
Jonathon Quick <jonathon@spliff.li>
2023-02-10 00:15:20 +0000

    made functions to save and read pip news data so it doesn't have to be mined from the tick data more than once (Since that is slow)

--- analyze_data.py
+++ analyze_data.py
@@ -2,7 +2,10 @@ import pandas as pd
 from datetime import datetime, timedelta
 import os
 import math
-import sys
+import json
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 DEFAULT_TIME_DELTAS = {
     "1s": timedelta(seconds=1),
@@ -29,12 +32,32 @@ DEFAULT_TIME_DELTAS = {
     "15m": timedelta(minutes=15),
 }
 
+def datetime_to_str(dt: datetime, microseconds=False):
+    if not microseconds:
+        return dt.strftime("%Y-%m-%d %H:%M:%S")
+    elif microseconds:
+        return dt.strftime("%Y-%m-%d %H:%M:%S:%f")
+
+
+def str_to_datetime(dt_str: str):
+    try:
+        return datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S")
+    except:
+        return datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S:%f")
+
 
 def read_news_data(haawks_id):
     for filename in os.listdir('./news_data'):
         if filename.startswith(haawks_id):
             news_data = pd.read_csv(f"./news_data/{filename}")
             return news_data
+
+
+def save_news_data(haawks_id, news_data):
+    for filename in os.listdir('./news_data'):
+        if filename.startswith(haawks_id):
+            print(f"saving to: news_data/{filename}")
+            news_data.to_csv(f"./news_data/{filename}", index=False)
 
 
 def get_decimal_places(num: float):
@@ -119,10 +142,19 @@ def get_relative_price_movements(prices_per_time_delta, release_time_price, deci
         difference_bid = round(bid - release_ask, decimal_places)
         price_movements[price] = (difference_ask, difference_bid)
 
-    for time in price_movements:
-        print(f"{time}:- Ask: {price_movements[time][0]:.5f} Bid: {price_movements[time][1]:.5f}")
-
     return price_movements
+
+
+def cross_reference_pips_with_news_data(news_data, pip_data):
+    news_pip_data = {}
+
+    for timestamp in pip_data:
+
+        for index, row in news_data.iterrows():
+            if timestamp == row['Timestamp']:
+                deviation = row['Deviation']
+                news_pip_data[timestamp] = {"deviation": deviation, "pips": pip_data[timestamp]}
+    return news_pip_data
 
 
 def get_pip_movements(price_movements, decimal_places):
@@ -136,34 +168,161 @@ def get_pip_movements(price_movements, decimal_places):
     return pip_movements
 
 
-def get_pip_movements_for_indicator(haawks_id, symbol):
-    news_data = read_news_data(haawks_id)
-    news_data = news_data.iloc[:5]  # FOR TESTING, shortens the dataframe to be quicker
+def read_tick_data(symbol, release_datetime):
+    release_date_hyphenated = release_datetime.strftime("%Y-%m-%d")
+    start_time_hyphenated = (release_datetime - timedelta(minutes=5)).strftime('%H-%M-%S')
+    end_time_hyphenated = (release_datetime + timedelta(minutes=15)).strftime('%H-%M-%S')
+    for filename in os.listdir("./tick_data"):
+        if filename == f"{symbol}__{release_date_hyphenated}__{start_time_hyphenated}_{end_time_hyphenated}.csv":
+            tick_data = pd.read_csv(f"./tick_data/{filename}")
+            return tick_data
 
-    pip_data = {}
-    row_count = news_data.shape[0]
+    raise ValueError(f"File {symbol}__{release_date_hyphenated}__{start_time_hyphenated}_{end_time_hyphenated}.csv doesn't exist")
 
-    for index, row in news_data.iterrows():
-        release_datetime = datetime.strptime((row['Timestamp']), "%Y-%m-%d %H:%M:%S")
-        release_date_hyphenated = release_datetime.strftime("%Y-%m-%d")
-        start_time_hyphenated = (release_datetime - timedelta(minutes=5)).strftime('%H-%M-%S')
-        end_time_hyphenated = (release_datetime + timedelta(minutes=15)).strftime('%H-%M-%S')
 
-        for filename in os.listdir("./tick_data"):
-            if filename == f"{symbol}__{release_date_hyphenated}__{start_time_hyphenated}_{end_time_hyphenated}.csv":
-                tick_data = pd.read_csv(f"./tick_data/{filename}")
+def get_indicator_info(haawks_id):
+    indicators = pd.read_excel("haawks-indicator-shortlist.xlsx")
+    for index, row in indicators.iterrows():
+        if str(row['Id']) == haawks_id:
+            cols = indicators.columns
+            indicator_info = {}
+            for index, value in enumerate(row.values):
+                indicator_info[cols[index]] = value
+            return indicator_info
+    raise ValueError("Invalid haawks_id")
+
+
+def save_news_pip_data(haawks_id, symbol, pip_data: dict):
+    # pip_data.to_csv(f"{symbol}")
+    indicator_info = get_indicator_info(haawks_id)
+    event_title = indicator_info['inv_title']
+    inv_id = indicator_info['inv_id']
+    start_datetime_str = list(pip_data.keys())[-1]
+    end_datetime_str = list(pip_data.keys())[0]
+
+    name_formatted = event_title.replace(" ", "_").replace(".", "")
+    dir_name = f"{haawks_id}_{inv_id}_{name_formatted}"
+    start_date_str = start_datetime_str.split(" ")[0]
+    end_date_str = end_datetime_str.split(" ")[0]
+    new_filename = f"{symbol}__{start_date_str}_{end_date_str}.json"
+
+    if not os.path.exists(os.path.join(os.getcwd(), f"analysis_data/{dir_name}/pip_data")):
+        os.makedirs(os.path.join(os.getcwd(), f"analysis_data/{dir_name}/pip_data"))
+
+    for existing_filename in os.listdir(f"./analysis_data/{dir_name}/pip_data/"):
+        if existing_filename.startswith(symbol):
+            os.remove(f"./analysis_data/{dir_name}/pip_data/{existing_filename}")
+            
+    file = open(f"./analysis_data/{dir_name}/pip_data/{new_filename}", "w")
+    json.dump(pip_data, file, indent=4)
+    file.close()
+
+
+def read_news_pip_data(haawks_id, symbol):
+    indicator_info = get_indicator_info(haawks_id)
+    event_title = indicator_info['inv_title']
+    inv_id = indicator_info['inv_id']
+
+    for dir_name in os.listdir("./analysis_data"):
+        if dir_name.startswith(haawks_id):
+            for filename in os.listdir(f"./analysis_data/{dir_name}/pip_data/"):
+                if filename.startswith(symbol):
+                    file = open(f"./analysis_data/{dir_name}/pip_data/{filename}")
+                    data = json.load(file)
+                    file.close()
+                    start_datetime = str_to_datetime(list(data.keys())[-1])
+                    end_datetime = str_to_datetime(list(data.keys())[0])
+
+                    return {
+                        "data_exists": True,
+                        "data": data,
+                        "start": start_datetime,
+                        "end": end_datetime,
+                    }
                 break
+    return {
+        "data_exists": False
+    }
 
-        print(f"Mining tick data from {release_date_hyphenated} ({str(int(index) + 1)}/{row_count}):")
-        prices = get_prices_at_time_deltas(release_datetime, tick_data)
-        release_price = get_release_time_price(release_datetime, tick_data)
-        decimal_places = get_decimal_places_from_tick_data(tick_data)
-        relative_price_movements = get_relative_price_movements(prices, release_price, decimal_places)
-        pip_movements = get_pip_movements(relative_price_movements, decimal_places)
+def mine_pip_data_from_ticks(news_data, symbol, release_datetime):
 
-        pip_data[release_datetime.strftime("%Y-%m-%d %H:%M:%S")] = pip_movements
+    row_count = news_data.shape[0]
+    pip_data = {}
 
-    return pip_data
+    tick_data = read_tick_data(symbol, release_datetime)
+
+    prices = get_prices_at_time_deltas(release_datetime, tick_data)
+    release_price = get_release_time_price(release_datetime, tick_data)
+    decimal_places = get_decimal_places_from_tick_data(tick_data)
+    relative_price_movements = get_relative_price_movements(prices, release_price, decimal_places)
+    pip_movements = get_pip_movements(relative_price_movements, decimal_places)
+
+    return pip_movements
+
+
+def sort_news_pip_data_by_timestamp(news_pip_data):
+
+    keys = list(news_pip_data.keys())
+    keys.sort(reverse=True)
+    sorted_dict = {i: news_pip_data[i] for i in keys}
+
+    return sorted_dict
+
+
+def load_news_pip_data(haawks_id, news_data, symbol):
+
+    # news_data = news_data.iloc[:7]  # FOR TESTING, shortens the dataframe to be quicker
+    indicator_info = get_indicator_info(haawks_id)
+
+    news_pip_data = {}
+    row_count = news_data.shape[0]
+    start_datetime = str_to_datetime(news_data.loc[row_count-1]['Timestamp'])
+    end_datetime = str_to_datetime(news_data.loc[0]['Timestamp'])
+    timestamps_to_mine = []
+
+    for timestamp in news_data["Timestamp"].items():
+        timestamps_to_mine.append(str_to_datetime(timestamp[1]))
+
+    print(f"Loading news pip data for {indicator_info['inv_title']}: {start_datetime} - {end_datetime}")
+
+    local_data = read_news_pip_data(haawks_id, symbol)
+    if local_data['data_exists']:
+        local_data_start = local_data['start']
+        local_data_end = local_data['end']
+
+        for timestamp in list(local_data['data'].keys()):
+            timestamp = str_to_datetime(timestamp)
+            if timestamp in timestamps_to_mine:
+                timestamps_to_mine.remove(timestamp)
+
+        news_pip_data = local_data['data']
+        if len(timestamps_to_mine) == 0:
+            print(f"Local data exists for {len(local_data['data'].keys())}/{row_count} releases. Reading data from file...")
+            return news_pip_data
+        else:
+            print(f"Local data exists for {len(local_data['data'].keys())}/{row_count} releases. The remaining {row_count - len(local_data['data'])} will be mined from raw tick data")
+    else:
+        print("No local data exists, so it will be mined...")
+
+    for index, timestamp in enumerate(timestamps_to_mine):
+        print(f"\rMining pip data from {timestamp} ({index+1}/{len(timestamps_to_mine)})", end="", flush=True)
+        data = mine_pip_data_from_ticks(news_data, symbol, timestamp)
+        timestamp_str = datetime_to_str(timestamp)
+        news_pip_data.setdefault(timestamp_str, {}).setdefault('pips', data)
+
+        for index, row in news_data.iterrows():
+            if row['Timestamp'] == timestamp_str:
+                try:
+                    news_pip_data[timestamp_str]['deviation'] = row['Deviation']
+                    break
+                except KeyError:
+                    news_pip_data[timestamp_str]['deviation'] = None
+
+    news_pip_data = sort_news_pip_data_by_timestamp(news_pip_data)
+    print("\nsaving mined data to file")
+    save_news_pip_data(haawks_id, symbol, news_pip_data)
+
+    return news_pip_data
 
 
 def get_deviation(actual, forecast):
@@ -188,26 +347,13 @@ def convert_news_data_to_float(haawks_id: str, data_str: str):
     return data
 
 
-def cross_reference_pips_with_news_data(haawks_id, pip_data):
-    news_data = read_news_data(haawks_id)
-    news_pip_data = {}
-
-    for timestamp in pip_data:
-
-        for index, row in news_data.iterrows():
-            if timestamp == row['Timestamp']:
-                deviation = row['Deviation']
-                news_pip_data[timestamp] = {"deviation": deviation, "pips": pip_data[timestamp]}
-    return news_pip_data
-
-
-def calc_deviations_for_indicator(haawks_id):
+def calc_all_deviations_for_indicator(haawks_id):
     news_data = read_news_data(haawks_id)
     row_count = news_data.shape[0]
-    news_data = news_data.loc[:, ~news_data.columns.str.contains('Unnamed')] # Remove unnamed column
+    news_data = news_data.loc[:, ~news_data.columns.str.contains('Unnamed')]  # Remove unnamed column
 
     for index, row in news_data.iterrows():
-        print(f"\rCalculating deviation for: {row['Timestamp']} ({str(index+1)}/{row_count})", end="", flush=True)
+        print(f"\rCalculating deviation for: {row['Timestamp']} ({str(index + 1)}/{row_count})", end="", flush=True)
         try:
             actual = convert_news_data_to_float(haawks_id, row['Actual'])
             forecast = convert_news_data_to_float(haawks_id, row['Forecast'])
@@ -217,10 +363,8 @@ def calc_deviations_for_indicator(haawks_id):
             print(f"\nForecast data missing from {row['Timestamp']}... Skipping")
 
     print(news_data)
-    for filename in os.listdir('./news_data'):
-        if filename.startswith(haawks_id):
-            print(f"saving to: news_data/{filename}")
-            news_data.to_csv(f"./news_data/{filename}", index=False)
+    save_news_data(haawks_id, news_data)
+
 
 def calc_all_indicator_deviations():
     indicators = pd.read_excel("haawks-indicator-shortlist.xlsx")
@@ -229,9 +373,10 @@ def calc_all_indicator_deviations():
     row_count = indicators.shape[0]
 
     for index, row in indicators.iterrows():
-        print(f"Calculating deviations for: {row['inv_title']} ({str(index+1)}/{row_count})")
+        print(f"Calculating deviations for: {row['inv_title']} ({str(index + 1)}/{row_count})")
         haawks_id = str(row['Id'])
-        calc_deviations_for_indicator(haawks_id)
+        calc_all_deviations_for_indicator(haawks_id)
+
 
 def calc_mean_deviation(news_data):
     deviations = []
@@ -275,16 +420,20 @@ def calc_quantiles(data_list: list, quantile_count=5):
 
     a = 1 / quantile_count
     n = len(data_list)
-
     quantiles = {}
-    data_list.sort()
 
-    for x in range(quantile_count - 1):
-        q = round(a * (x +1), 4)
-        i = q * (n + 1)
-        index = math.floor(i)
-        quantiles[str(q)] = data_list[index]
-    return quantiles
+    if n > quantile_count - 1:
+        data_list.sort()
+
+        for x in range(quantile_count - 1):
+            q = round(a * (x + 1), 4)
+            i = q * (n + 1)
+            index = math.floor(i)
+            quantiles[str(q)] = data_list[index]
+
+        return quantiles
+    else:
+        raise ValueError("Data list is smaller than the quantile count. Cannot calculate")
 
 
 def calc_deviation_quantiles(news_data, quantile_count=5):
@@ -309,24 +458,95 @@ def calc_deviation_quantiles(news_data, quantile_count=5):
             neg_deviations.append(value)
             all_deviations.append(value)
 
-    pos_quantiles = calc_quantiles(pos_deviations, quantile_count)
-    neg_quantiles = calc_quantiles(neg_deviations, quantile_count)
-    all_dev_quantiles = calc_quantiles(all_deviations, quantile_count)
+    try:
+        pos_quantiles = calc_quantiles(pos_deviations, quantile_count)
+        neg_quantiles = calc_quantiles(neg_deviations, quantile_count)
+        all_dev_quantiles = calc_quantiles(all_deviations, quantile_count)
 
-    return {
-        "positive": pos_quantiles,
-        "negative": neg_quantiles,
-        "combined": all_dev_quantiles,
-    }
+        return {
+            "positive": pos_quantiles,
+            "negative": neg_quantiles,
+            "combined": all_dev_quantiles,
+        }
+    except ValueError:
+        pass
 
 
+def calc_and_save_trigger_levels(haawks_id):
+    """This just gets the quantiles for the deviations and populates the news data with it.
+    The levels it outputs will be adjusted manually to be more round numbers"""
+    news_data = read_news_data(haawks_id)
+    indicators = pd.read_excel("haawks-indicator-shortlist.xlsx")
+    for index, row in indicators.iterrows():
+        if str(row['Id']) == haawks_id:
+            title = row['inv_title']
+    if 'Deviation' in news_data.columns:
 
+        quantiles = calc_deviation_quantiles(news_data)
+
+        if quantiles is not None:
+            triggers = {}
+
+            for count, value in enumerate(quantiles['combined']):
+                triggers[str(f"trigger_{count + 1}")] = quantiles['combined'][value]
+
+            for index, row in indicators.iterrows():
+                if str(row['Id']) == haawks_id:
+                    for trigger in triggers:
+                        indicators.loc[index, trigger] = triggers[trigger]
+
+                    # print(indicators.loc[index])
+                    break
+            indicators.to_excel("haawks-indicator-shortlist.xlsx", index=False)
+            return triggers
+    else:
+        print(f"Deviations missing from {title}... Skipping")
+
+
+def calc_and_save_all_trigger_levels():
+    indicators = pd.read_excel("haawks-indicator-shortlist.xlsx")
+    # indicators = indicators.loc[28:]
+    row_count = indicators.shape[0]
+    for index, row in indicators.iterrows():
+        print(f"\rCalculating triggers for indicator {str(index + 1)}/{row_count}: {row['inv_title']}")
+
+        calc_and_save_trigger_levels(str(row['Id']))
+
+
+def read_triggers(haawks_id):
+    indicators = pd.read_excel("haawks-indicator-shortlist.xlsx")
+    for index, row in indicators.iterrows():
+        if str(row['Id']) == haawks_id:
+            triggers = {}
+            i = 1
+            while True:
+                try:
+                    triggers[f"trigger_{i}"] = row[f"trigger_{i}"]
+                    i += 1
+                except KeyError:
+                    break
+            break
+    return triggers
+
+
+def calc_news_pip_stats(news_pip_data, triggers):
+    news_pip_stats = {}
+
+    for timestamp in news_pip_data:
+        for trigger in triggers:
+
+            for time_delta in news_pip_data[timestamp]['pips']:
+                pass
 
 
 news_data = read_news_data("10000")
+triggers = read_triggers("10000")
 # mean_deviations = calc_median_deviations(news_data)
 # calc_deviations_for_indicator("10000")
-# quantiles = calc_deviation_quantiles(news_data)
-calc_all_indicator_deviations()
-pip_data = get_pip_movements_for_indicator("10000", "EURUSD")
-news_pip_data = cross_reference_pips_with_news_data("10000", pip_data)
+# calc_all_indicator_deviations()
+# calc_and_save_all_trigger_levels()
+# read_news_pip_data("10000", "EURUSD")
+news_pip_data = load_news_pip_data("10000", news_data, "EURUSD")
+# calc_news_pip_stats(news_pip_data, triggers)
+
+# news_pip_data = cross_reference_pips_with_news_data("10000", pip_data)
